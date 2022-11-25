7174f6e4a89ac41c9d86470b8808b2b8619fb29d
Paras Upadhyay <paras13upadhyay@gmail.com>
2022-11-25 19:25:02 +0530

    Chapter2 Exercise1 {CipherSchools}

---
+++ Chapter2_Exercise1.py
@@ -0,0 +1,10 @@
+#                                 Chapter 2 Exercise1
+"""
+Ask user to input three numbers and you have to print average of three numbers using 
+string formmating.
+"""
+# Code -->
+num1 = int(input("Enter first number"))
+num2 = int(input("Enter second number"))
+num3 = int(input("Enter third number"))
+print(f"Average of the numbers {num1},{num2},{num3} is {(num1+num2+num3)/3}")
