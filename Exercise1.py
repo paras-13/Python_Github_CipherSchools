2b1f00e5e643e49e4958a7d71d02640dd348dd96
Paras Upadhyay <paras13upadhyay@gmail.com>
2022-11-25 19:07:54 +0530

    Exercise 1 Cipher Schools

---
+++ Exercise1.py
@@ -0,0 +1,14 @@
+
+#**********************print these following lines************************
+# this is \\ double backslash
+# these are /\/\/\/\/\ mountains
+# he is     awesome (use escape sequence sequence in place of normal spaces)
+# \" \n \t \'  (print this as an output)
+
+print("this is \\\\ double slash")
+print("these are /\\/\\/\\/\\/\\ mountains")
+print("he is \t awesome")
+print("\\\"")
+print("\\n")
+print("\\t")
+print("\\'")
