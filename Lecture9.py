295ca8dc36906afb9325746f3de2bd0fa767ced0
Paras Upadhyay <paras13upadhyay@gmail.com>
2022-12-08 00:12:42 +0530

    Lecture9 Escape sequence (Cipher Schools)

---
+++ Lecture9.py
@@ -0,0 +1,7 @@
+# About print function
+
+print("Hello World")       #Can be written in single or double quote
+print('hello world')
+print("Hello 'world' world")
+print('Hello "world" world' )
+print("I am Paras")
