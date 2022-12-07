5723a8a5ecdca786168597e76504fea8ca6a23d9
Paras Upadhyay <paras13upadhyay@gmail.com>
2022-12-08 00:14:12 +0530

    Lecture11 Commets in Python (Cipher Schools)

---
+++ Lecture11.py
@@ -0,0 +1,8 @@
+# Comments in Python
+# Commments cam be single line, multiline or inline
+
+print("LinA\nLineB\nLineC")    # \n for new line  
+print("name \tParas")            # \t for tab space
+print("This is backslash\\")     
+print("This is double backslash\\\\")   # \\is for backslash
+print("Hell\blo")                   # \b for backslash
