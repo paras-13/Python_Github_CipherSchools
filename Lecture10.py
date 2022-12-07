a1259eb073edc5f7fbbd44cf07496e9db6a02d1d
Paras Upadhyay <paras13upadhyay@gmail.com>
2022-12-08 00:13:40 +0530

    Lecture 10 (Application of Escape sequence)

---
+++ Lecture10.py
@@ -0,0 +1,9 @@
+# About Escape sequence
+
+print("hello \"world\" world")   
+print("I\'m Paras")
+print("LinA\nLineB\nLineC")      
+print("name \tParas")            
+print("This is backslash\\")     
+print("This is double backslash\\\\")
+print("Hell\blo")                
