005e3d2e32d28e84dd2643a173c4b6d9a63d8182
Paras Upadhyay <paras13upadhyay@gmail.com>
2022-12-08 00:16:10 +0530

    Lecture17 Python as calculator (CipherSchools)

---
+++ Lecture17.py
@@ -0,0 +1,18 @@
+# Python as Calculator
+
+print(2+3*4)
+print(2/4)
+print(4/2)
+print(4//2)
+print(2//4)
+print(2**3)
+print(2**0.5)
+print(round(2**0.5,4))
+print(2**3/2*6-4*(3-4/2))
+print(8%2)
+print((2+3)/2)
+print(5/2)
+print((2+3)*5/2%6)
+print(12.5%6)
+print(2**3**2)
+print(2**9)
