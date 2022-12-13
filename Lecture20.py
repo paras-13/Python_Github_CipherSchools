2140461871ab47da66c1913ffd018944fd40171e
Paras Upadhyay <paras13upadhyay@gmail.com>
2022-12-13 23:46:35 +0530

    Lecture20 Inputs from the user

---
+++ Lecture20.py
@@ -0,0 +1,10 @@
+# String Concatenation
+
+first_name = "Harshit"
+Last_name = "Vashistha"
+full_name = first_name + Last_name
+# print(full_name)
+# print(first_name + 3) error
+# print(first_name + "3") no error
+# print(first_name + str(3)) no error
+print(first_name*3)
